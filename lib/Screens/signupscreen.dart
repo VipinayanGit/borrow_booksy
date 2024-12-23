7c8c4ca10206e7e220806096dd785acb24a6a5f5
VipinayanGit <145025543+VipinayanGit@users.noreply.github.com>
2024-12-24 01:26:44 +0530

    added textfields

--- lib/Screens/signupscreen.dart
+++ lib/Screens/signupscreen.dart
@@ -12,24 +12,67 @@ class _signupscreenState extends State<signupscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("Signup page"),
+        title: Text("Signup page"),
         centerTitle: true,
       ),
-      
       body: SafeArea(
-        child:Center(
+          child: SingleChildScrollView(
+        child: Center(
           child: Column(
             children: [
               Padding(
                 padding: const EdgeInsets.all(10),
-                child: Image.asset("assets/logo.png",
-                height: 150,
-                width: 150,),
+                child: Image.asset(
+                  "assets/logo.png",
+                  height: 150,
+                  width: 150,
+                ),
+              ),
+              SizedBox(height: 40),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+                child: TextField(
+                  decoration: InputDecoration(
+                      // fillColor: Colors.white,
+                      filled: true,
+                      labelText: "Username",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10)),
+                      
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                 padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+                child: TextField(
+                  decoration: InputDecoration(
+                    //fillColor: Colors.white,
+                    filled: true,
+                    labelText: "password",
+                    border: InputBorder.none
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                 padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+                child: TextField(
+                  
+                  decoration: InputDecoration(
+                    //fillColor: Colors.white,
+                    filled: true,
+                    labelText: "ID",
+                    border: InputBorder.none
+                  ),
+                ),
               ),
             ],
           ),
-        ) ),
-
+        ),
+      )),
     );
   }
 }
